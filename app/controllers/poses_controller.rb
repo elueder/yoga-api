b11d17878b6a189013f4167d67b0ab1baec3cbce
elueder <lizzie.lueder@gmail.com>
2018-07-06 13:02:45 -0400

    Make poses#update and poses#delete only accessable to owner

--- app/controllers/poses_controller.rb
+++ app/controllers/poses_controller.rb
@@ -41,7 +41,7 @@ class PosesController < ProtectedController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_pose
-      @pose = Pose.find(params[:id])
+      @pose = current_user.poses.find(params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
